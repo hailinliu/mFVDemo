b5ffc8799756a30fb95f414cd2caecdc7e5c720f
e <1135231523@qq.com>
2018-01-29 16:37:01 +0800

    新增一个main2activity,添加了一个方法

---
+++ src/main/java/com/iflytek/mfvdemo/util/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.iflytek.mfvdemo.util;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.iflytek.mfvdemo.R;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        findViewById(R.id.edt_group_id);
+    }
+}
