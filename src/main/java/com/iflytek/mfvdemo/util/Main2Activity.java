db7166694c0bb7c2bbb3f44e6651f55b8028959c
e <1135231523@qq.com>
2018-01-29 16:38:19 +0800

    新增一个main2activity,添加了一个方法

--- src/main/java/com/iflytek/mfvdemo/util/Main2Activity.java
+++ src/main/java/com/iflytek/mfvdemo/util/Main2Activity.java
@@ -2,6 +2,7 @@ package com.iflytek.mfvdemo.util;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.iflytek.mfvdemo.R;
 
@@ -11,6 +12,6 @@ public class Main2Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        findViewById(R.id.edt_group_id);
+        EditText editText = (EditText)findViewById(R.id.edt_group_id);
     }
 }
